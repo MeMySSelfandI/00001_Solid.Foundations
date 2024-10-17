917675082697526890f1f14c228266d913d591eb
MeMySSelfandI <avgaen@gmail.com>
2024-10-17 03:45:31 -0500

    My First deployed contract, by FAMO made, id est, hello_world

---
+++ hello_world.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.24;  // Compiler version is defined here by ME!
+
+// The logic of my contract begins here!
+contract hello_world {
+
+    // Declaring the content of my greeting variable with Unicode support.
+    string public greeting = unicode"卐 ► Hello from EducatETH BOGOTÁ, D.C. v3.0 ◄ 卐 [!!!] ";
+    
+}
