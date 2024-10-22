c498b65bbc70d5b42f847d1a61efd7ec45bfbb11
MeMySSelfandI <avgaen@gmail.com>
2024-10-21 22:24:31 -0500

    Dynamic Hello World v1 from BOGOTÁ

--- dynamic_hello_world.sol
+++ dynamic_hello_world.sol
@@ -4,8 +4,8 @@ pragma solidity 0.8.24;
 
 contract dynamic_hello_world {
 
-    string dyn_Greeting = unicode"卐 ► Dynamic Hello World ◄ 卐";
-    string public sta_Greeting = unicode"卐 ► Initial greeting on deployment ◄ 卐";
+    string dyn_Greeting = unicode"卐 ► Dynamic Hello World v1 from BOGOTÁ ◄ 卐";
+    string public sta_Greeting = unicode"卐 ► Static HW greeting on deployment from BOGOTÁ ◄ 卐";
 
     function readGreeting() public view returns (string memory) {
         return dyn_Greeting;
