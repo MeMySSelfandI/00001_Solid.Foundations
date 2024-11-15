52526f34ec4a9907fd8d9a683d39484cc90575e2
MeMySSelfandI <avgaen@gmail.com>
2024-11-15 03:55:05 -0500

    A smart contract called «My_FAMO_contract_by_Austin_Griffith»

--- My_FAMO_contract_by_Austin_Griffith.sol
+++ My_FAMO_contract_by_Austin_Griffith.sol
@@ -1,5 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0 <0.9.0;
 
-// import
+// import "@openzeppelin/contracts/access/Ownable.sol";
+// https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
 import "hardhat/console.sol";
+
+contract My_FAMO_contract_by_Austin_Griffith {
+    event SetPurpose(address sender, string purpose);
+    string public purpose = "Building Unstoppable Apps!!!";
+    address public owner = 0x9663CFe87d8c06E861d0D5fcBd358d50988a4b7E;
+    constructor() {
+        // what should we do on deploy?
+    }
+
+    function setPurpose(string memory newPurpose) public {
+        purpose = newPurpose;
+        console.log(msg.sender,"set purpose to",purpose);
+        emit SetPurpose(msg.sender, purpose);
+    }
+}
